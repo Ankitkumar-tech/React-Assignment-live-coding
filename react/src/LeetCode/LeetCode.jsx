4286810dc2536195d974563c7187f86cabf7066b
ankit <ankit8287670719@gmail.com>
2024-01-22 18:32:51 +0530

    fetched data

--- react/src/LeetCode/LeetCode.jsx
+++ react/src/LeetCode/LeetCode.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const LeetCode = () => {
     const [studentCode, setStudentCode] = useState('');
-    const [data, setData] = useState([] || null)
+    let [data, setData] = useState([] || null)
     const [username, setUsername] = useState({})
     const userNamesList = ['vishaal351', 'ankitkumar-tech', "arajAnkit", "mideasy2"];
+    const [filterdata, setFilterData] = useState([] || null)
 
+    // useEffect(()=>{
+    //     fetchLeetCode()
+    // },[])
     async function fetchLeetCode(userName) {
         try {
             const res = await fetch(`https://leetcode-api-faisalshohag.vercel.app/${userName}`);
@@ -27,10 +31,29 @@ const LeetCode = () => {
         }
     }
 
+    // function 
+
     function handleAutoSearch() {
         userNamesList.forEach((userName) => {
             fetchLeetCode(userName);
         });
+    }
+
+    function filtermintomax() {
+        let sortedData = [...data];
+        sortedData.sort((a, b) => b.totalSolved - a.totalSolved);
+        setData(sortedData);
+    }
+    function filtermaxtomin() {
+        let sortedData = [...data];
+        sortedData.sort((a, b) => a.totalSolved - b.totalSolved);
+        setData(sortedData);
+    }
+
+    function formatTimestamp(timestamp) {
+        const date = new Date(timestamp * 1000);
+        // console.log(data.getFullYear())
+        return date.getFullYear(); // Adjust the format as needed
     }
     console.log(data, "data")
     return (
@@ -44,9 +67,19 @@ const LeetCode = () => {
             />
             <button>Sort</button>
             <button onClick={handleSearch}>Search</button>
+            <input type="date" />
             <button onClick={handleAutoSearch}>Auto Search</button>
+            <button onClick={filtermintomax}>Ghatte se badhte huye </button>
+            <button onClick={filtermaxtomin}>Badhte se ghatte huye</button>
             {data.map((e) =>
-                <div>{e?.totalSolved}</div>
+                <>
+                    <div>{e?.totalSolved}</div>
+                    {e?.recentSubmissions.map((z) => <> <p>{z.title}</p>
+
+                        <p>{formatTimestamp(z?.timestamp)}</p>
+                        <p>{z?.name}</p>
+                    </>)}
+                </>
             )}
         </div>
     );
